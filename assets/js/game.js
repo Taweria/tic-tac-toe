45672aa99ba81c7c4517a6bc5d225c49279da975
Fourneaux Thibaut <fourneaux.thibaut@gmail.com>
2023-09-08 16:45:32 +0200

    fix bg change

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,19 @@
-let scorePlayer1 = 0;
-    scorePlayer2 = 0;
+let scorePlayer1 = 0,
+    scorePlayer2 = 0,
 
-    b1 = document.getElementById("b1");
-    b2 = document.getElementById("b2");
-    b3 = document.getElementById("b3");
-    b4 = document.getElementById("b4");
-    b5 = document.getElementById("b5");
-    b6 = document.getElementById("b6");
-    b7 = document.getElementById("b7");
-    b8 = document.getElementById("b8");
-    b9 = document.getElementById("b9");
+    b1 = document.getElementById("b1"),
+    b2 = document.getElementById("b2"),
+    b3 = document.getElementById("b3"),
+    b4 = document.getElementById("b4"),
+    b5 = document.getElementById("b5"),
+    b6 = document.getElementById("b6"),
+    b7 = document.getElementById("b7"),
+    b8 = document.getElementById("b8"),
+    b9 = document.getElementById("b9"),
     arr = [b1, b2, b3, b4, b5, b6, b7, b8, b9];
 
-for (let i = 0; i < arr.length; i++) {
-       arr[i].addEventListener("click", changeColor(arr[i].id));
-}
-
-
-function changeColor(domId) {
-    document.getElementById(domId).style.backgroundColor = '#DEE5FF';
-}
+arr.forEach(element => {
+    element.addEventListener("click", () => {
+        element.style.backgroundColor = "#DEE5FF";
+    });
+});
